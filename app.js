607b083c8e88de3a9e3f7ea57c3ae13dbb1e69dd
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-05 15:14:22 +0700

    LAB 1.3: Send respond

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const http = require('http')
 
 // Tạo ra 1 biến để lưu trữ server // vế sau là đã khởi tạo 1 serer
 const server = http.createServer((req, res) => {
-    console.log(req)
-})
-// MỞ Port cho SERVER // con số 3000 có thể thay đổi mặc định sẽ là 
+    console.log(req.url, req.method, req.headers)
+    // res gửi phản hồi
+    res.setHeader('Content-Type', 'text/html')
+    res.write("<html>")
+    res.write("<header><title>My First Page</title></header>")
+    res.write("<body><h1>Hello from my Node.js server!</h1></body>")
+    res.write("</html>")
+    res.end()
 
+})
+
+// MỞ Port cho SERVER // con số 3000 có thể thay đổi mặc định sẽ là http://localhost:3000/
 server.listen(3000)
