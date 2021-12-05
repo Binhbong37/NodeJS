b567bf544ab3c4a204d39cf27e655f184c9f5e0a
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-05 15:47:03 +0700

    Lab 1.5: Chuyển hướng request 2

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-
+// Video 33
 const http = require('http')
-
+const fs = require('fs')
 const server = http.createServer((req, res) => {
-    // video 32: Routing request
+    // LAB 1.5: 
     const url = req.url;
+    const method = req.method
     if(url === '/') {
         res.setHeader('Content-Type', 'text/html')
         res.write("<html>")
@@ -12,6 +13,12 @@ const server = http.createServer((req, res) => {
         <input type = 'text' name='message'> <button type='submit'>Send</button>
         </form></body>`)
         res.write("</html>")
+        return res.end()
+    }
+    if(url === "/message" && method === "POST") {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end()
     }
     res.setHeader('Content-Type', 'text/html')
