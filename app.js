5f231aa1715d3a618198f74ec1e66b1b0b4fb554
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-07 08:20:14 +0700

    Lab 1.7: Sử dụng Hệ thống Mô-đun Node

--- app.js
+++ app.js
@@ -1,44 +1,7 @@
-// Video 34:
-const http = require('http')
-const fs = require('fs')
-const server = http.createServer((req, res) => {
-    
-    const url = req.url;
-    const method = req.method
-    if(url === '/') {
-        res.setHeader('Content-Type', 'text/html')
-        res.write("<html>")
-        res.write("<header><title>My First Page</title></header>")
-        res.write(`<body><form action='/message' method='POST'>
-        <input type = 'text' name='message'> <button type='submit'>Send</button>
-        </form></body>`)
-        res.write("</html>")
-        return res.end()
-    }
-    // LAB 1.6: Đọc BODY của Request
-    if(url === "/message" && method === "POST") {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk)
-            body.push(chunk)
-        });
-        req.on('end',() => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        })
-       
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end()
-    }
-    res.setHeader('Content-Type', 'text/html')
-    res.write("<html>")
-    res.write("<header><title>My First Page</title></header>")
-    res.write("<body><h1>Hello from my Node.js server!</h1></body>")
-    res.write("</html>")
-    res.end()
 
-})
+const http = require('http')
+const routes = require("./routes")
+console.log(routes.someText)
+const server = http.createServer(routes.handler)
 
 server.listen(3000)
