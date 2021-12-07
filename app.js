d55f70694556a987d0de3f2aa4113474c07d36f3
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-07 09:32:22 +0700

    Lab 2.1: Cai dat Express.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 const http = require('http')
-const routes = require("./routes")
-console.log(routes.someText)
-const server = http.createServer(routes.handler)
+// Video 59: cai va import express
+const express = require("express")
+const app = express()
+const server = http.createServer(app)
 
 server.listen(3000)
