bde3b613d543534636a2598fc0d70b2f722589fc
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-07 14:30:03 +0700

    Lab 2.12: Trả lại trang 404

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const adminRoutes = require("./routes/admin")
 const shopRoutes = require("./routes/shop")
 const app = express()
 const bodyParser = require("body-parser")
+const path = require("path")
 
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -11,6 +12,6 @@ app.use('/admin',adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found </h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 app.listen(3000)
