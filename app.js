e868ca1dd1657309346956f592999a6d030eba4c
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-07 09:47:34 +0700

    Lab 2.2: Cai dat Middleware

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 
 const http = require('http')
-// Video 59: cai va import express
 const express = require("express")
 const app = express()
+// Video 60:
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next()
+})
+app.use((req, res, next) => {
+    console.log('In another middleware')
+})
 const server = http.createServer(app)
 
 server.listen(3000)
