2ae343c7121c29e085034cdeb06211219f1f999a
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-07 10:26:34 +0700

    Lab 2.4: Xử lý các Route khác nhau

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 
-const http = require('http')
 const express = require("express")
 const app = express()
-// Video 60:
-app.use((req, res, next) => {
-    console.log('In the middleware');
+
+// Luon chay
+app.use('/', (req, res, next) => {
+    console.log("This always runs!")
     next()
 })
-app.use((req, res, next) => {
+// Middleware 1
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware');
+    res.send("<h1>The Add product page</h1>")
+})
+// Middleware 2
+app.use('/', (req, res, next) => {
     console.log('In another middleware');
     res.send("<h1>Hello from Express</h1>")
 })
-const server = http.createServer(app)
 
-server.listen(3000)
+app.listen(3000)
