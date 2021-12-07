ff5e09c081ce90e3c4769a37979aff1c6161638d
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-07 11:42:57 +0700

    Lab 2.9: Phan luong duong dan

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
     
-    res.send(`<form action="/product" method="POST">
+    res.send(`<form action="/admin/add-product" method="POST">
     <input type="text" name="title"/>
     <button type="submit"> Add Product </button>
     </form>`)
 })
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body)
     res.redirect("/")
 })
