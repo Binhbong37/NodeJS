dc3d911ad80ea551980c3a180c40999d5d2a6253
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-07 11:33:26 +0700

    Lab 2.8: Them trang xu ly loi 404

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const shopRoutes = require("./routes/shop")
 const app = express()
 const bodyParser = require("body-parser")
 
-
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(adminRoutes)
-
 app.use(shopRoutes)
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found </h1>')
+})
 app.listen(3000)
