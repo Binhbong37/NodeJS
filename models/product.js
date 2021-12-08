8ebcee7bcdfd36533732ad8c4757c59b62b1ddea
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-08 21:43:40 +0700

    Lab 4.6: Cấu trúc lại code lưu trữ tệp

--- models/product.js
+++ models/product.js
@@ -1,7 +1,20 @@
 // Save to file
 const fs = require("fs")
 const path = require("path")
-
+// tao duong dan global
+const pa = path.join(path.dirname(require.main.filename),
+'data',
+'product.json'
+)
+const getProductsFromFile = cb => {
+        fs.readFile(pa, (err, fileContent) => {
+            if (err) {
+                cb([]);
+            } else {
+                cb(JSON.parse(fileContent))
+            }
+        })
+}
 // Khá giống class com cần viết HOA chữ cái đầu
 module.exports = class Product {
     constructor(title) {
@@ -9,33 +22,15 @@ module.exports = class Product {
     }
 
     save() {
-        const pa = path.join(path.dirname(require.main.filename),
-        'data',
-        'product.json'
-        )
-        fs.readFile(pa, (err, fileContent) => {
-            let products = []
-            if (!err) {
-                products = JSON.parse(fileContent)
-            }
+        getProductsFromFile(products => {
             products.push(this)
             fs.writeFile(pa, JSON.stringify(products), (err) => {
                 console.log(err)
             })
         })
-
     }
 
     static fetchAll(cb) {
-        const pa = path.join(path.dirname(require.main.filename),
-        'data',
-        'product.json'
-        )
-        fs.readFile(pa, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent))
-        })
+        getProductsFromFile(cb)
     }
 }
