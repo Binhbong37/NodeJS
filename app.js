6400dae11816ef1cd82eba86788d6958e87d795c
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-08 20:03:06 +0700

    Lab 4.2: Hoàn thiện controller

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-
+// import file tu controller
+const errorController = require("./controllers/error")
 const bodyParser = require("body-parser")
 const path = require("path");
 
@@ -20,7 +21,5 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin',adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: "Not Found"})
-})
+app.use(errorController.get404)
 app.listen(3000)
