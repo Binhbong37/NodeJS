e94e0a42bba96394c51fb40f3dce8d716e8acd4b
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-09 08:19:34 +0700

    Lab 4.8: trích xuất các thông số động

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,13 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+// Phần sẽ lấy id sản phẩm
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId
+  console.log(prodId)
+  res.redirect("/")
+}
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
