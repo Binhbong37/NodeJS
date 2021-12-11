b14355084150e71a7c9b938c461a99a8fd88fa64
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-11 08:24:49 +0700

    Lab 5.8: Kết nối với Cơ sở dữ liệu

--- util/database.js
+++ util/database.js
@@ -1,13 +1,7 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize")
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '1234567',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-
-module.exports = pool.promise();
+const sequelize = new Sequelize('node-complete', 'root', '1234567',{
+    dialect: 'mysql',
+    host:'localhost'
+})
+module.exports = sequelize
