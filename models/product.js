79eacb7b0e79b06bf4c7334f108d524c42d41a58
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-11 08:06:52 +0700

    Lab 5.7: Tìm nạp một Sản phẩm với Điều kiện where

--- models/product.js
+++ models/product.js
@@ -26,6 +26,9 @@ module.exports = class Product {
     return db.execute('SELECT * FROM products')
   }
 
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
+  }
 
 
 }
