121ce4051d0ba4f49ec4a4a2402fcdff3f156f30
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-12 09:38:32 +0700

    Lab 5.22: Tạo & Tìm nạp Giỏ hàng

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ Cart.belongsToMany(Product, {through: CartItem})
 Product.belongsToMany(Cart, {through: CartItem})
 
 sequelize
-.sync({force: true})
-// .sync()
+// .sync({force: true})
+.sync()
 .then((result) => {
     return User.findById(1)
     
@@ -60,6 +60,9 @@ sequelize
 })
 .then(user => {
     // console.log(user);
+    return user.createCart()
+})
+.then(cart => {
     app.listen(3000);
 })
 .catch(err => {
--- controllers/shop.js
+++ controllers/shop.js
@@ -48,22 +48,37 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.fetchAll(products => {
-      const cartProducts = []
-      for(product of products) {
-        const cartProductData = cart.products.find(prod => prod.id === product.id )
-        if(cartProductData) {
-          cartProducts.push({productData: product, qty: cartProductData.qty})
-        }
-      }
+  req.user.getCart()
+  .then(cart => {
+    return cart.getProducts()
+    .then(product => {
       res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts 
-      });
+          path: '/cart',
+          pageTitle: 'Your Cart',
+          products: product 
+        });
     })
+    .catch(err => console.log("Post ben trong"))
   })
+  .catch(err => console.log('Loi tu post card'))
+  // Cart.getCart(cart => {
+  //   Product.fetchAll(products => {
+  //     const cartProducts = []
+  //     for(product of products) {
+  //       const cartProductData = cart.products.find(prod => prod.id === product.id )
+  //       if(cartProductData) {
+  //         cartProducts.push({productData: product, qty: cartProductData.qty})
+  //       }
+  //     }
+  //     res.render('shop/cart', {
+  //       path: '/cart',
+  //       pageTitle: 'Your Cart',
+  //       products: cartProducts 
+  //     });
+  //   })
+  // })
+
+
 };
 
 exports.postCart = (req, res, next) => {
