d23967494a5e094e838fed905a307fcf2ed8fe41
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-12 08:19:33 +0700

    Lab 5.18: Tạo và quản lý người dùng tạm thời

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const errorController = require('./controllers/error');
 
 const sequelize = require("./util/database")
 
+const Product = require("./models/product")
+const User = require("./models/users")
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,14 +21,38 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findById(1)
+    .then(user => {
+        req.user = user
+        next()
+    })
+    .catch(err => console.log("Loi tai file app"))
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
+User.hasMany(Product)
+
+sequelize
+// .sync({force: true})
+.sync()
 .then((result) => {
-    // console.log(result);
+    return User.findById(1)
+    
+})
+.then(user => {
+    if(!user) {
+        return User.create({name:"Max", email:"abc@gmail.com"})
+    }
+    return user
+})
+.then(user => {
+    // console.log(user);
     app.listen(3000);
 })
 .catch(err => {
