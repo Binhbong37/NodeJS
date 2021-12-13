aeb40b1496555c66aa6eb744a376b095d9c21ff0
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-13 14:00:56 +0700

    Lab 6.5: Tạo được sản phẩm

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,12 +15,9 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user.createProduct({
-    title: title,
-    imageUrl: imageUrl,
-    price: price,
-    description: description
-  })
+  const product = new Product(title, price, description, imageUrl);
+  product
+  .save()
   .then((result) => {
     res.redirect('/admin/products')
   })
