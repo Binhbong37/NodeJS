ce377401c169c29e22523d7109dbd9ff93eec964
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-14 13:55:50 +0700

    Lab 7.7: Xóa sản phẩm

--- controllers/admin.js
+++ controllers/admin.js
@@ -94,7 +94,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
   .then(result => {
     console.log('DELETED')
     res.redirect('/admin/products')
