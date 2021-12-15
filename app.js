d4ac163010b5cc31540c76a673258ff6d9cf1c3f
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-15 10:01:10 +0700

    Lab 8.7: Sử dụng MongoDB để lưu trữ Session

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const session = require("express-session")
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session)
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
+const MONGODB_URI = "mongodb://localhost:27017"
 
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,7 +25,14 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: "my secret", resave: false, saveUninitialized: false }))
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+  )
 
 app.use((req, res, next) => {
   User.findById('61b8421fcb081c04a8a1846c')
@@ -37,9 +50,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb://localhost:27017"
-  )
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
