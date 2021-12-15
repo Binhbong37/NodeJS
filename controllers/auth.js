82436f5f2bb36cc2b0928cd58a76e1eb02143a71
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-15 14:52:04 +0700

    Lab 8.17: Thêm chức năng đăng nhập

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,14 +19,31 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  const email = req.body.email;
+  const password = req.body.password
+  User.findOne({email: email})
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if(!user) {
+        return res.redirect("/login")
+      }
+      bcrypt
+      .compare(password, user.password)
+      .then(doMatch => {
+        if(doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save(err => {
+            console.log("K co loi");
+            res.redirect("/")
+          });
+        }
+        res.redirect("/login")
+        console.log("MAN RUA")
+      })
+      .catch(err => {
+        console.log("Loi k so sanh dc mat khau Control: ")
+        res.redirect("/login")
+      })
     })
     .catch(err => console.log(err));
 };
