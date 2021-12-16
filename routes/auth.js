ff0fa90938ce68f45596a0ec112c90bc959e301d
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-16 09:36:11 +0700

    Lab 9.4: Thêm trình kiểm tra dữ liệu

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -12,14 +12,20 @@ router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
 
 router.post('/signup',
-check('email')
-.isEmail().withMessage('Please enter a valid email')
-.custom((value, {req}) => {
+[
+    check('email')
+    .isEmail().withMessage('Please enter a valid email')
+    .custom((value, {req}) => {
     if(value === "test@test.com") {
         throw new Error('this email address if forbidden')
     }
     return true
-}),
+    }),
+    body('password', "Password at least 5 character only text and number !")
+    .isLength({min: 5})
+    .isAlphanumeric()
+    
+],
 
 authController.postSignup);
 
