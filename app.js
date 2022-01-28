b37390d6f06608961c535808973e69a7bb1fd4f6
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-01-28 08:21:10 +0700

    LAB 10.4, 10.5: Cấu hình Multer, lọc tập tin bằng Mimetype

--- app.js
+++ app.js
@@ -12,33 +12,33 @@ const multer = require('multer');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = "mongodb://localhost:27017"
+const MONGODB_URI = 'mongodb://localhost:27017';
 const app = express();
 const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: 'sessions'
+    uri: MONGODB_URI,
+    collection: 'sessions',
 });
 const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  }
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Math.random() + '-' + file.originalname);
+    },
 });
 const fileFilter = (req, file, cb) => {
-  if(
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg'  ||
-    file.mimetype === 'image/jpeg') {
-    cb(null, true)
-  } else {
-    cb(null, false)
-  }
-}
-
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -48,41 +48,43 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
+app.use(
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({
-    secret: 'my secret',
-    resave: false,
-    saveUninitialized: false,
-    store: store
-  })
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store,
+    })
 );
 app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
 });
 
 app.use((req, res, next) => {
-  // throw new Error('Sync Dummy');
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then(user => {
-      if (!user) {
+    // throw new Error('Sync Dummy');
+    if (!req.session.user) {
         return next();
-      }
-      req.user = user;
-      next();
-    })
-    .catch(err => {
-      next(new Error(err));
-    });
+    }
+    User.findById(req.session.user._id)
+        .then((user) => {
+            if (!user) {
+                return next();
+            }
+            req.user = user;
+            next();
+        })
+        .catch((err) => {
+            next(new Error(err));
+        });
 });
 
 app.use('/admin', adminRoutes);
@@ -94,20 +96,20 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  console.log(req.cookie)
-  res.status(500).render('500', {
-    pageTitle: 'Error!',
-    path: '/500',
-    isAuthenticated: req.cookie ? 'true' : 'false'
-  });
+    console.log(req.cookie);
+    res.status(500).render('500', {
+        pageTitle: 'Error!',
+        path: '/500',
+        isAuthenticated: req.cookie ? 'true' : 'false',
+    });
 });
 
 mongoose
-  .connect(MONGODB_URI)
-  .then(result => {
-    console.log("Ket noi voi MONGOOSE !!!")
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .connect(MONGODB_URI)
+    .then((result) => {
+        console.log('Ket noi voi MONGOOSE !!!');
+        app.listen(3000);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
