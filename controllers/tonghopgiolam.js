e8ca824d115080f6cb52816dcbf5465de50911da
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-02-27 11:26:43 +0700

    Sửa logic cho phần tổng hợp giờ làm, sửa css file đó

--- controllers/tonghopgiolam.js
+++ controllers/tonghopgiolam.js
@@ -4,11 +4,22 @@ const moment = require('moment');
 exports.getTongHopGioLam = (req, res, next) => {
     TimeTable.find().then((result) => {
         result = result.map((inf) => {
+            const overTime = (inf.updatedAt - inf.createdAt) / 36e5;
+            let timeOver = 0;
+            if (overTime > 8) {
+                timeOver = overTime - 8;
+            }
             return {
                 dayWork: moment(inf.createdAt).format('ll'),
-                startDate: moment(inf.createdAt).format('LT'),
-                endDate: moment(inf.updatedAt).format('LT'),
-                place: inf.place,
+                startDate: moment(inf.createdAt).format('lll'),
+                endDate: moment(inf.updatedAt).format('lll'),
+                overTime: Math.round(timeOver * 100) / 100,
+                place:
+                    inf.place === '1'
+                        ? 'Công ty'
+                        : inf.place === '2'
+                        ? 'Ở nhà'
+                        : 'KH',
             };
         });
         res.render('shop/tonghopgiolam', {
